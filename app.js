8402004e5b06d8e77ee6fdb62a4fef3d0f8b9f15
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-19 20:56:50 +0800

    加入Catching Errors的method

--- app.js
+++ app.js
@@ -3,19 +3,7 @@ let fetchPromise = fetch(
 );
 console.log(fetchPromise);
 
-fetchPromise.then((response) => {
-  //   console.log(fetchPromise);
-  //   console.log(response);
-
-  // response.json()將response內，文字的部分轉成JSON(JavaScript Object Notation)
-  // .json() method os also asynchronous function
-  // 所以它也會return Promise object
-  response.json().then((data) => {
-    console.log(data);
-  });
-});
-// 然而，上面的寫法會產生callback hell
-// 所以改寫如下：
 fetchPromise
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => console.log(data))
+  .catch((e) => console.log(e));
