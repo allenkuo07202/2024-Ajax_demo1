9fed3ca0d91ad723f3d46d318386b5edca12b2a6
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-19 18:24:36 +0800

    同步、異步釋例2

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-console.log("開始執行程式碼");
-setTimeout(() => console.log("2秒後出現"), 2000);
-console.log("最後一行");
+let max = 1000000;
 
-// JavaScript中，絕大多數的function皆屬於synchronous function
-function hello() {
-  let name = "Wilson";
-  console.log("hello" + name);
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return n > 1;
 }
 
-console.log("你好");
-hello();
-console.log("你好2");
+const random = (max) => Math.floor(Math.random() * max);
+
+function generatePrimes(quota) {
+  const primes = [];
+  while (primes.length < quota) {
+    const candidate = random(max);
+    if (isPrime(candidate)) {
+      primes.push(candidate);
+    }
+  }
+  return primes;
+}
+
+const output = document.querySelector("div#output");
+const button = document.querySelector("#generate");
+button.addEventListener("click", (e) => {
+  const quota = document.querySelector("#number");
+  const primes = generatePrimes(quota.value);
+  output.innerHTML = primes;
+});
