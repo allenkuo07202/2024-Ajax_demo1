d74c7861d3d7726ad65d3785a0a8dfce17f1c334
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-20 21:57:25 +0800

    製作mutex解決Race Condition

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let balance = 0; // shared resource
+let mutex = Promise.resolve(); // return a fulfilled promise
+// 一開始就要return a fulfilled promise的原因是
+// 第一次執行mutex.then()的時候，我們就要直接執行then後面的程式碼
 
 const randomDelay = () => {
   // return value is a Promise
@@ -20,24 +23,38 @@ async function saveBalance(value) {
 }
 
 async function sellGrapes() {
-  const balance = await loadBalance();
-  // 本來loadBalance()是return一個Promise，但我們有用await，所以會直接return balance
-  console.log(`賣葡萄前，帳戶金額為${balance}`);
-  const newBalance = balance + 50;
-  await saveBalance(newBalance);
-  console.log(`賣葡萄後，帳戶金額為${newBalance}`);
+  mutex = mutex.then(async () => {
+    const balance = await loadBalance();
+    // 本來loadBalance()是return一個Promise，但我們有用await，所以會直接return balance
+    console.log(`賣葡萄前，帳戶金額為${balance}`);
+    const newBalance = balance + 50;
+    await saveBalance(newBalance);
+    console.log(`賣葡萄後，帳戶金額為${newBalance}`);
+  });
+  return mutex; // 因為async function要return一個Promise，所以在這裡return mutex
 }
 
 async function sellOlives() {
-  const balance = await loadBalance();
-  console.log(`賣橄欖前，帳戶金額為${balance}`);
-  const newBalance = balance + 50;
-  await saveBalance(newBalance);
-  console.log(`賣橄欖後，帳戶金額為${newBalance}`);
+  mutex = mutex.then(async () => {
+    const balance = await loadBalance();
+    console.log(`賣橄欖前，帳戶金額為${balance}`);
+    const newBalance = balance + 50;
+    await saveBalance(newBalance);
+    console.log(`賣橄欖後，帳戶金額為${newBalance}`);
+  });
+  return mutex;
 }
 
 async function main() {
-  await Promise.all([sellGrapes(), sellOlives()]);
+  await Promise.all([
+    sellGrapes(),
+    sellOlives(),
+    sellOlives(),
+    sellOlives(),
+    sellGrapes(),
+    sellGrapes(),
+    sellGrapes(),
+  ]);
   const balance = await loadBalance();
   console.log(`賣葡萄與橄欖後的帳戶金額是${balance}`);
 }
