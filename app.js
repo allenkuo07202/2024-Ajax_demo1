9608ae6252b96379e7c994320b939b55a1fe2794
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-20 15:26:38 +0800

    Node.js Event Loop釋例

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-async function myFunc() {
-  return 100;
-}
+console.log("start"); // sync
 
-let result = myFunc();
-console.log(result);
-result.then((data) => console.log(data));
+process.nextTick(function () {
+  console.log("nextTick1");
+});
 
-async function fetchProduct() {
-  try {
-    const response = await fetch(
-      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
-    );
-    const data = await response.json();
-    console.log(data);
-  } catch (e) {
-    console.log(e);
-  }
-}
+setTimeout(function () {
+  console.log("setTimeout");
+}, 0);
+// call the constructor 是一個 sync function
+new Promise(function (resolve, reject) {
+  console.log("promise");
+  resolve("resolve");
+}).then(function (result) {
+  console.log("promise then");
+});
+// IIFE
+(async function () {
+  console.log("async");
+})();
 
-fetchProduct();
-// 我們把.then()的語法，改成await的語法，那要如何處理錯誤？
-// 把上面的部分放進try，下面再加catch
+setImmediate(function () {
+  console.log("setImmediate");
+});
+
+process.nextTick(function () {
+  console.log("nextTick2");
+});
+
+console.log("end");
